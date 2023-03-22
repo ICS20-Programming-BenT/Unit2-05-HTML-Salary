6ed593410c3e87ddac5b9c53230f96b79dc4cf1b
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-03-22 14:51:10 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2023 Ben Thomson. All rights reserved.
+//
+// Created by: Ben Thomson
+// Created on: 03/21/2023
+// This file contains the JS functions for index.html
+
+"use strict";
+
+// Declare tax amount as the constant
+const TAX_RATE = 0.2005;
+
+//This function will calculate someone's salary.
+function payButtonClicked () {
+ 
+  // Get the user input
+  let hoursWorked = parseFloat(document.getElementById('hoursWorked').value)
+  let hourlyRate = parseFloat(document.getElementById('hourlyRate').value)
+
+  // Perform mathematical equations
+  let totalPay = hoursWorked * hourlyRate
+  let taxesPaid = TAX_RATE * totalPay
+	let netIncome = totalPay - taxesPaid 
+  
+  // Display the results
+  document.getElementById('total-earnings').innerHTML = "Your pay will be $" + netIncome.toFixed(2)
+  document.getElementById('tax-amount').innerHTML = "The government will take $" + taxesPaid.toFixed(2)
+}
